6d322f989405ccd90ecd2ace0e4e53ca1bf04e52
Ante <241960689+antekem15-glitch@users.noreply.github.com.>
2026-05-30 12:33:30 +0200

    add background change on mouseover. add change to random color

--- myscript.js
+++ myscript.js
@@ -14,11 +14,30 @@ function gridMaker(num) {
         newBox.style.margin = "0 0px";
         grid.appendChild(newBox)
         count = document.getElementById("container").children.length;
+        newBox.classList.add("pixel")
         
 
     }
 
+
+
 }
+function getRandomColor() {
+    const r = Math.floor(Math.random()*256)
+    const g = Math.floor(Math.random()*256)
+    const b = Math.floor(Math.random()*256)
+    return `rgb(${r}, ${g}, ${b})`;
+
+
+}
+
+
+document.getElementById("container").addEventListener("mouseover", function(event){
+    if (event.target.classList.contains("pixel")) {
+        event.target.style.backgroundColor = getRandomColor();
+
+}});
+
 gridMaker()
 
 
