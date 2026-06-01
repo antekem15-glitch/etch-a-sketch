650ec7c3196c5f255945b7580eff71be2b21cbaa
Ante <241960689+antekem15-glitch@users.noreply.github.com.>
2026-06-01 12:02:04 +0200

    add buttons for color change.

--- myscript.js
+++ myscript.js
@@ -20,9 +20,9 @@ function gridMaker(num) {
         grid.appendChild(newBox)
         count++;
         newBox.classList.add("pixel")
-       
-
+    
     }
+
 
 }
 function getRandomColor() {
@@ -32,25 +32,39 @@ function getRandomColor() {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
+let currentColorMode="random";
+
 document.getElementById("container").addEventListener("mouseover", function(event){
     if (event.target.classList.contains("pixel")) {
+        if (currentColorMode === "random") {
         event.target.style.backgroundColor = getRandomColor();
+        } else if (currentColorMode === "black") {
+            event.target.style.backgroundColor = "black";
+        } else if (currentColorMode === "red") {
+            event.target.style.backgroundColor = "red";
+        }
 
 }});
 
 gridMaker(16)
 
-const button = document.querySelector("button").addEventListener("click", function () {
-   const input = prompt("Enter a number between 1 and 100", 16);
-   if (!input) return;
+    const button = document.querySelector("#btn").addEventListener("click", function () {
+    let input = prompt("Enter a number between 1 and 100", 16);
+     if (!input) return;
 
-    const size = Number(input);
+    let size = Number(input);
     const grid = document.getElementById("container")
     let count = document.getElementById("container").children.length;
-    if (size > 100) { 
-        alert("Keep it below 100") 
-        return;}
-  while (count > 0) {
+    while (size > 100) { 
+        input = prompt ("Keep it below 100!")
+        if(!input) return;
+        size = Number(input)
+        
+        }
+
+  
+  
+while (count > 0) {
     grid.removeChild(grid.lastChild)
     --count;
   }
@@ -58,3 +72,19 @@ const button = document.querySelector("button").addEventListener("click", functi
   gridMaker(size)
         
     });
+
+    document.getElementById("reset").addEventListener("click", function(){
+        const allPixel = document.querySelectorAll(".pixel");
+        allPixel.forEach (pixel => {
+            pixel.style.backgroundColor = "White";
+        })
+    })
+
+    document.getElementById("black").addEventListener("click", function() {
+        currentColorMode = "black";})
+
+    document.getElementById("red").addEventListener("click", function() {
+        currentColorMode = "red";})
+
+    document.getElementById("random").addEventListener("click", function() {
+        currentColorMode = "random";})
